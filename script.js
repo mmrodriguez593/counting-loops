f9a1832fe9ef50058729173ca9ea971c6c39ff44
Michael Rodriguez <mmrodriguez593@hotmail.com>
2022-07-13 15:44:10 -0400

    final commit

---
+++ script.js
@@ -0,0 +1,17 @@
+for (let i = 0; i <= 10; i++ ){
+    console.log(i)
+}
+for (let i = 10; i >= 0; i--){
+    console.log(i)
+}
+
+let a = 0
+while (a < 10){
+    a++;
+console.log(a)
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let number of numbers) {
+    console.log(number)
+}
